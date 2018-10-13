ea39494bb0989072e58a6523bc3bffd59b791355
tremueller <38141613+tremueller@users.noreply.github.com>
2018-10-13 11:40:00 -0700

    Add files via upload

---
+++ YouTubeGraph/YouTubeGraph/YouTubeLibrary/Video.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeLibrary
+{
+    public class Video
+    {
+        public string Title { get; set; }
+        //public string channel { get; set; }
+        //public int views { get; set; }
+        public TimeSpan Duration { get; set; }
+        public List<string> Tags { get; set; }
+        public string Category { get; set; }
+
+        public Video (string title, TimeSpan duration, List<string> tags, string category)
+        {
+            Title = title;
+            Duration = duration;
+            Tags = tags;
+
+            Category = category;
+        }
+
+    }
+}
